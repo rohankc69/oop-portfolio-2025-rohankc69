03e766f01b41b65861febd6d094838717a2c868b
rohankc69 <kcrohanraj@gmail.com>
2025-02-21 21:36:56 +0545

    Final Commit

---
+++ PersonalDetails.java
@@ -0,0 +1,13 @@
+public class PersonalDetails {
+    public static void main(String[] args) {
+        String name = "Rohan Raj K.C.";
+        String address = "Kathmandu";
+        int age = 18;
+        String phoneNumber = "+977 9818673268";
+
+        System.out.println("Name: " + name);
+        System.out.println("Address: " + address);
+        System.out.println("Age: " + age);
+        System.out.println("Phone Number: " + phoneNumber);
+    }
+}
